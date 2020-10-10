545ed90a39b4a4ef718f50305f1e0e3773921fb0
Christopher Blount <christopher.blount@outlook.com>
2020-10-09 21:01:38 -0500

    Added a hello from Chris!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello my name is Bryan");
+
+        System.out.println("Hello Bryan, my name is Chris");
     }
 }
